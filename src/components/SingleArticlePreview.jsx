a4011bd28d739f092dc3b5740acd7ff8898fd253
Kevin Zehner <kevzehner@gmail.com>
2023-12-12 10:30:09 +0000

    implements PR feedback

--- src/components/SingleArticlePreview.jsx
+++ src/components/SingleArticlePreview.jsx
@@ -14,28 +14,46 @@ const SingleArticlePreview = (props) => {
       <div className="article-top-container">
         <p className="article-info">Topic: {article.topic}</p>
         <p className="article-info">by {article.author}</p>
-        <p className="article-info">Created at: {article.created_at}</p>
       </div>
       <p className="article-info"> {truncateArticle(article.body)}</p>
       <div className="lower-article-container">
-        <p className="article-info">{article.comment_count}</p>
-        <p>
+        <div className="comment-container">
+          <p className="article-info">{article.comment_count}</p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"
+            width="20px"
+            height="20px"
+            viewBox="0 0 32 32"
+            version="1.1"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-            />
+            <title>comment-4</title>
+            <desc>Created with Sketch Beta.</desc>
+            <defs></defs>
+            <g
+              id="Page-1"
+              stroke="none"
+              stroke-width="1"
+              fill="none"
+              fill-rule="evenodd"
+              sketch:type="MSPage"
+            >
+              <g
+                id="Icon-Set"
+                sketch:type="MSLayerGroup"
+                transform="translate(-308.000000, -255.000000)"
+                fill="#000000"
+              >
+                <path
+                  d="M327.494,279.633 L324,284 L320.506,279.633 C314.464,278.355 309.992,273.863 309.992,268.501 C309.992,262.146 316.264,256.994 324,256.994 C331.736,256.994 338.008,262.146 338.008,268.501 C338.008,273.863 333.536,278.355 327.494,279.633 L327.494,279.633 Z M324,255 C315.163,255 308,261.143 308,268.72 C308,274.969 312.877,280.232 319.542,281.889 L324,287.001 L328.459,281.889 C335.123,280.232 340,274.969 340,268.72 C340,261.143 332.837,255 324,255 L324,255 Z"
+                  id="comment-4"
+                  sketch:type="MSShapeGroup"
+                ></path>
+              </g>
+            </g>
           </svg>
-        </p>
-
+        </div>
         <div className="votes-container">
           <svg
             xmlns="http://www.w3.org/2000/svg"
